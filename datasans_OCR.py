d8a9d2c218a118df40317d370ce8a8fec3f8357e
Rio Nur Arifin <59592379+arifinrio95@users.noreply.github.com>
2023-09-28 08:57:39 +0700

    Update datasans_OCR.py

--- datasans_OCR.py
+++ datasans_OCR.py
@@ -79,7 +79,7 @@ def ocr_analyze(ocr_output):
     messages = [
         {"role": "system", "content": "Aku akan menulis ulang text kamu dengan format yang baik."},
         # {"role": "user", "content": f"""Buat 2 bagian. Pertama, tuliskan text dari hasil OCR saya yang berantakan agar terbaca dengan mudah. Kedua, analisa data tersebut dengan basis keilmuan yang kuat dan ilmiah, serta berikan referensinya. Output OCR:  {ocr_output}."""}
-        {"role": "user", "content": f"""Rapikan text dari hasil OCR saya yang berantakan agar terbaca dengan mudah dengan format yang baik. Text OCR:  {ocr_output}."""}
+        {"role": "user", "content": f"""Rapikan text dari hasil OCR yang berantakan ini agar terbaca dengan mudah dengan format yang rapi. Text OCR:  {ocr_output}."""}
     ]
 
     response = openai.ChatCompletion.create(
